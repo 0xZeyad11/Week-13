c82d3b42f3db63f41130cbbb259017c77fbf2e6f
Zeyad Hesham <zorro.zizo3@gmail.com>
2025-11-22 20:20:22 +0200

    add login and verify tokens from the cookies

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -13,6 +13,8 @@ export const protect = async (req ,res ,next) => {
     try {
         console.log("jwt value: " , token)
         const JWT_SECRET = process.env.JWT_SECRET;
+        if(!JWT_SECRET)
+            throw new Error("Failed loading the jwt secret")
         const decoded = jwt.verify(token , JWT_SECRET);
         console.log(decoded);
         req.user = decoded ; 
